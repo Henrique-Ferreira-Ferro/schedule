8ad5dcbb93aef75207bbc97c7b359166890b14ff
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-03-07 10:38:14 -0300

    Testando

--- src/main/java/com/schedule/demo/VotationApplication.java
+++ src/main/java/com/schedule/demo/VotationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VotationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VotationApplication.class, args);
+		SpringApplication.run(VotationApplication.class, args)
 	}
 
 }
