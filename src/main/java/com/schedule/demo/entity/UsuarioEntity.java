249a6bbf3ac2dacd174bcc41edd2b878dddf8e8a
Gui Pinheiro <101673957+GuiRuchell@users.noreply.github.com>
2024-03-28 11:16:21 -0300

    Update UsuarioEntity.java

--- src/main/java/com/schedule/demo/entity/UsuarioEntity.java
+++ src/main/java/com/schedule/demo/entity/UsuarioEntity.java
@@ -1,6 +1,9 @@
 package com.schedule.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class UsuarioEntity {
