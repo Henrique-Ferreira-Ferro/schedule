da7cb44ce3157f2dbc20ae0de3038bba449947c0
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-04-07 17:44:48 -0300

    criação da funcionalidade de cadastrar usuario e busca de usuario

--- src/main/java/com/schedule/demo/controller/UsuarioController.java
+++ src/main/java/com/schedule/demo/controller/UsuarioController.java
@@ -1,16 +1,37 @@
 package com.schedule.demo.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.schedule.demo.entity.UsuarioEntity;
+import com.schedule.demo.service.UsuarioService;
+
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/user")
 public class UsuarioController {
 	
-	@GetMapping
-	public String getHello() {
-		return "Hello";
+	@Autowired
+	private UsuarioService service;
+	
+	@GetMapping("{id}")
+	public Optional<UsuarioEntity> getUser(@PathVariable Long id) {
+		return service.getUser(id);
 	}
 	
+	
+	@PostMapping("/create")
+	public UsuarioEntity createUser(@RequestBody UsuarioEntity user) {
+		return service.createUser(user);
+		
+	}
+	
+	
 }
+
--- src/main/java/com/schedule/demo/service/UsuarioService.java
+++ src/main/java/com/schedule/demo/service/UsuarioService.java
@@ -1,9 +1,28 @@
 package com.schedule.demo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.schedule.demo.entity.UsuarioEntity;
+import com.schedule.demo.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	
+	public UsuarioEntity createUser(UsuarioEntity user) {
+		return repository.save(user);
+		
+	}
+	
+	public Optional<UsuarioEntity> getUser(Long id) {
+		return repository.findById(id);
+	}
 	
 	
 }
