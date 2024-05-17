0ccc7d9b5cd1b47c198e67f91607604481fcf692
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-05-16 22:29:19 -0300

    Pequena alteração no service

--- src/main/java/com/schedule/demo/controller/PautaController.java
+++ src/main/java/com/schedule/demo/controller/PautaController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class PautaController {
 	}
 	
 	@GetMapping("/{id}")
-	public Optional<PautaEntity> findPautaById(Long id){
+	public Optional<PautaEntity> findPautaById(@PathVariable Long id){
 		return service.findPautaById(id);
 	}
 	
