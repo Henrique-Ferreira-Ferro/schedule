c25c60e2a54a1900d522e856a4ddcc8cb5498fd5
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-06-27 15:52:29 -0300

    Inserção do Swagger e inicio da documentação do sistema

---
+++ src/main/java/com/schedule/demo/config/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package com.schedule.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfiguration {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Api Schedule").description("É uma pauta de votação :)"));
+				
+		
+	}
+	
+	
+}
